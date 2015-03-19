361f8bf0ce29a0979e291a139e74436e5ca4a873
Adnan Shafique <ad11105033@gmail.com>
2015-03-19 15:48:47 +0500

    commit
    3:45  room :P

--- app.js
+++ app.js
@@ -60,80 +60,11 @@ angular.module('app', ['flowChart', ])
 	var chartDataModel = {
 
 		nodes: [
-			{
-				name: "Node 1",
-				id: 0,
-				x: 50,
-				y: 400,
-				width: 350,
-				inputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-				outputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-			},
-
-			{
-				name: "Node 2",
-				id: 1,
-				x: 400,
-				y: 200,
-				inputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-				outputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-			},
-
+			
 		],
 
 		connections: [
-			{
-				source: {
-					nodeID: 0,
-					connectorIndex: 1,
-				},
-
-				dest: {
-					nodeID: 1,
-					connectorIndex: 2,
-				},
-			},
-
+			
 
 		]
 	};
