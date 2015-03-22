e6425fbd8e176169108b145e45ca4f28721d792c
Adnan Shafique <ad11105033@gmail.com>
2015-03-22 08:24:15 +0500

    noodes
    :P

--- process.php
+++ process.php
@@ -24,12 +24,21 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "<br>Error: " . $sql . "<br>" . $conn->error;
 }
+/*
+{ "nodes": [ { "name": "Adnan Shafique", "id": 10, "x": 282, "y": 684.0000267028809, "inputConnectors": [ { "name": "adasd" } ], "outputConnectors": [ { "name": "ashahsd" } ], "width": 250 }, { "name": "mad", "id": 11, "x": 267, "y": 71.99996566772461, "inputConnectors": [ { "name": "New connector" } ], "outputConnectors": [ { "name": "New connector" } ], "width": 250 } ], "connections": [ { "source": { "nodeID": 10, "connectorIndex": 0 }, "dest": { "nodeID": 11, "connectorIndex": 0 } }, { "source": { "nodeID": 11, "connectorIndex": 0 }, "dest": { "nodeID": 10, "connectorIndex": 0 } } ] }
+*/
+
 $conn->close();
-//$id=$map['nodes'];
-//for($id as $id)
-//{
-//   echo $id['nodes']." ".$id['name'];
-//}
-//
-//$status=$data['nodes'];
+$map_decod=json_decode($map,true);
+$data = array();
+$name = array();
+$id = array();
+                $nodes = $map_decod["nodes"];
+
+echo "NAME:-".$nodes[0]['name']."<br>";
+echo "ID:-".$nodes[0]['id']."<br>";
+$i_conn=$nodes[0]['inputConnectors'];
+echo "in connector 1-".$i_conn[0]['name']."<br>";
+$o_conn=$nodes[0]['outputConnectors'];
+echo "o_connector-1:-".$o_conn[0]['name']."<br>";
 ?>
