fd053e451cebcc27d76489a7af484ea95d638114
Adnan Shafique <ad11105033@gmail.com>
2015-03-23 15:28:49 +0500

    :P
    Adnan

--- process.php
+++ process.php
@@ -18,16 +18,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql_map = "INSERT INTO `concept-map-builder`.`concept-maps` (`map_id`, `map_data`, `map_created_by`, `created_on`) 
-VALUES (NULL, '$map', 'Adnan Shafique', CURRENT_TIMESTAMP)";
+$sql_map = "INSERT INTO `concept-map-builder`.`concept-maps` (`map_id`, `map_data`, `map_created_by`, `created_on`) VALUES (NULL,'$map', 'Adnan Shafique', CURRENT_TIMESTAMP)";
 
 if ($conn->query($sql_map) === TRUE) {
-    echo "<br><h4>New record created successfully</h4>";
+    $last_id = $conn->insert_id;
+    echo "New record created successfully. Last inserted map ID is: " . $last_id;
 } else {
-    echo "<br>Error: " . $sql_map . "<br>" . $conn->error;
+    echo "Error: " . $sql_map . "<br>" . $conn->error;
 }
 
-$conn->close();
+//$conn->close();
+
 $map_decod=json_decode($map,true);
 
 $nodes = $map_decod["nodes"];
@@ -35,9 +36,17 @@ $count_nodes=count($nodes);
 for($nodes_count=0;$nodes_count<$count_nodes;$nodes_count++)
 {
 echo "<br>NAME:-".$nodes[$nodes_count]['name']."<br>";
+    $name=$nodes[$nodes_count]['name'];
 echo "ID:-".$nodes[$nodes_count]['id']."<br>";
-//$sql_slct_max="SELECT MAX(map_id) FROM concept-maps";
-    
+    $id=$nodes[$nodes_count]['id'];
+$sql_node = "INSERT INTO `nodes`(`map_id`, `node_id`, `nodeID`, `nodeName`) VALUES ($last_id,NULL,$id,'$name')";
+if ($conn->query($sql_node) === TRUE) {
+    $last_id = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+} else {
+    echo "Error: " . $sql_node . "<br>" . $conn->error;
+}
+  
 //--------------input connectors--------------    
 $i_conn=$nodes[$nodes_count]['inputConnectors'];
 $count_i_conn=count($i_conn);
@@ -45,6 +54,16 @@ $count_i_conn=count($i_conn);
 for($i_conn_count=0;$i_conn_count<$count_i_conn;$i_conn_count++)
 {
     echo "in connector".$i_conn_count.":_".$i_conn[$i_conn_count]['name']."<br>";
+    $nme=$i_conn[$i_conn_count]['name'];
+    //INSERT INTO `input_connection`(`node_id`, `connector_code`, `connector_name`) VALUES ([value-1],[value-2],[value-3])
+    $sql_node = "INSERT INTO 'input_connection'(`node_id`, `connector_code`, `connector_name`) VALUES ($last_id,$i_conn_count,$nme)";
+if ($conn->query($sql_node) === TRUE) {
+    $last_inconnector = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_inconnector;
+} else {
+    echo "Error: " . $sql_node . "<br>" . $conn->error;
+}
+
 }
 //--------------output connectors--------------    
 $o_conn=$nodes[$nodes_count]['outputConnectors'];
@@ -53,6 +72,16 @@ $count_o_conn=count($o_conn);
 for($o_conn_count=0;$o_conn_count<$count_o_conn;$o_conn_count++)
 {
     echo "out connector-".$o_conn_count.":_".$i_conn[$o_conn_count]['name']."<br>";
+    $nme=$i_conn[$o_conn_count]['name'];
+    $sql_node = "INSERT INTO `output_connection`(`node_id`, `connector_code`, `connector_name`) VALUES ($last_id,$o_conn_count,$nme)";
+if ($conn->query($sql_node) === TRUE) {
+    $last_outconnector = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_outconnector;
+} else {
+    echo "Error: " . $sql_node . "<br>" . $conn->error;
+}
+
+    
 }
 
 }
